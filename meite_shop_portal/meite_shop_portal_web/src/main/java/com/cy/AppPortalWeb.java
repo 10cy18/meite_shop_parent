cae78e0a7ad813a8ef6a5353ac617e0f7289d04d
chen yong <cy986275730@qq.com>
2019-05-07 22:59:37 +0800

    注册 验证码

---
+++ meite_shop_portal/meite_shop_portal_web/src/main/java/com/cy/AppPortalWeb.java
@@ -0,0 +1,18 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@EnableEurekaClient
+//@EnableFeignClients
+public class AppPortalWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(AppPortalWeb.class, args);
+    }
+}
---
+++ meite_shop_portal/meite_shop_portal_web/src/main/java/com/cy/member/controller/RegisterController.java
@@ -0,0 +1,66 @@
+package com.cy.member.controller;
+
+import com.cy.member.controller.req.vo.RegisterVo;
+import com.cy.web.base.BaseWebController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class RegisterController extends BaseWebController {
+	private static final String MB_REGISTER_FTL = "member/register";
+	private static final String MEMBER_REGISTER_PAGE = "member/register";
+	/**
+	 * 跳转到登陆页面页面
+	 */
+	private static final String MB_LOGIN_FTL = "member/login";
+
+	/**
+	 * 跳转到注册页面
+	 * 
+	 * @return
+	 */
+	@GetMapping("/register")
+	public String getRegister() {
+		return MEMBER_REGISTER_PAGE;
+	}
+
+	/**
+	 * 跳转到注册页面
+	 * 
+	 * @return
+	 */
+	@PostMapping("/register")
+	public String postRegister(@ModelAttribute("registerVo") @Validated RegisterVo registerVo,BindingResult bindingResult, HttpSession httpSession, Model model) {
+		// 1.参数验证
+		if (bindingResult.hasErrors()) {
+			// 获取第一个错误!
+			String errorMsg = bindingResult.getFieldError().getDefaultMessage();
+			setErrorMsg(model, errorMsg);
+			return MB_REGISTER_FTL;
+		}
+		// 将VO转换DTO
+		/*UserInpDTO voToDto = MiteBeanUtils.voToDto(registerVo, UserInpDTO.class);
+		try {
+			String registCode = registerVo.getRegistCode();
+			BaseResponse<JSONObject> register = memberRegisterServiceFeign.register(voToDto, registCode);
+			if (!register.getCode().equals(Constants.HTTP_RES_CODE_200)) {
+				model.addAttribute("error", register.getMsg());
+				return MB_REGISTER_FTL;
+			}
+		} catch (Exception e) {
+			log.error(">>>>>", e);
+			model.addAttribute("error", "系统出现错误!");
+			return MB_REGISTER_FTL;
+		}*/
+
+		// 注册成功,跳转到登陆页面
+		return MB_LOGIN_FTL;
+	}
+}
