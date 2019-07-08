ca48ad11c4514f6dbf858cde27c0a67a1cc21029
cyever <986275730@qq.com>
2019-07-08 14:49:32 +0800

    集成swagger美化ui doc.html

--- meite_shop_basics/meite_shop_basics_springcloud_eureka/src/main/java/com/cy/app/AppEureka.java
+++ meite_shop_basics/meite_shop_basics_springcloud_eureka/src/main/java/com/cy/app/AppEureka.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 功能描述: 
+ *
+ * @param: eureka 服务端
+ * @return: 
+ * @author: cy
+ * @date: 2019/7/8 14:33
+ */
 @SpringBootApplication
 @EnableEurekaServer
-public class AppEuraka {
+public class AppEureka {
     public static void main(String[] args) {
-        SpringApplication.run(AppEuraka.class,args);
+        SpringApplication.run(AppEureka.class,args);
     }
 }
